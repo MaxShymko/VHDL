df91071b2335e40d6415d30ca98ea60a7c24f488
ProlificStark <shimko.max@mail.ru>
2017-11-08 20:29:19 +0300

    ant without random

--- course/main.vhd
+++ course/main.vhd
@@ -1,67 +1,117 @@
+Library IEEE, work;
+Use IEEE.std_logic_1164.all;
+Use work.types.all;
+Use STD.textio.all; -- file
 entity main is
-	port(clk : in bit);
+	port(clk : in std_logic;
+		ant : out ant_arr);
 end main;
 
 architecture struct of main is
 
-constant MATRIX_SIZE: natural := 5 + 2;
-
-type ant_arr is array (natural range 0 to 1) of integer range 0 to MATRIX_SIZE - 1;
-type pheromone_arr is array (natural range 0 to MATRIX_SIZE - 1, natural range 0 to MATRIX_SIZE - 1) of integer;
-type ant_near_arr is array (natural range 0 to 7) of ant_arr;
-
-signal ant, startPos : ant_arr;
-signal pheromone : pheromone_arr;
-signal goHome : boolean;
-
-begin
-	pheromone <= (
+signal next_ant : ant_arr := (0=>MATRIX_SIZE-2, 1=>MATRIX_SIZE-2); -- pos of ant
+signal startPos : ant_arr := (0=>MATRIX_SIZE-2, 1=>MATRIX_SIZE-2); -- anthill
+signal pheromone : pheromone_arr := (
 		0 => (others => -1),
 		1 => (1=>1,2=>2,3=>3,4=>1,5=>1,others => -1),
 		2 => (1=>1,2=>2,3=>4,4=>2,5=>3,others => -1),
 		3 => (1=>0,2=>2,3=>0,4=>5,5=>3,others => -1),
 		4 => (1=>0,2=>0,3=>0,4=>6,5=>0,others => -1),
 		5 => (1=>0,2=>0,3=>0,4=>3,5=>7,others => -1),
-		6=> (others => -1)
+		6 => (others => -1)
 	);
-	goHome <= true;
-	startPos <= (0=>MATRIX_SIZE - 2, 1=>MATRIX_SIZE - 2);
+signal eat : eat_arr := (
+	0 => (true, false,false,false,true ),
+	1 => (false,false,false,false,false),
+	2 => (false,false,true, false,false),
+	3 => (false,false,false,false,false),
+	4 => (true, false,false,false,false)
+	);
+signal goHome : boolean := false;
+
+begin
 
 	main_process: process(clk)
+
+	-- file declarations
+	variable outdata_line : line;
+	file output_data_file : text open write_mode is "results.txt";
+	--
+
 	variable ant_near : ant_near_arr;
 	variable best_way : natural range 0 to 7;
 
-
 	begin
 
-		if(clk'event and clk = '1') then
-			ant_near(0)(0) := ant(0) + 1; ant_near(0)(1) := ant(1) + 1;
-			ant_near(1)(0) := ant(0) + 1; ant_near(1)(1) := ant(1);
-			ant_near(2)(0) := ant(0);     ant_near(2)(1) := ant(1) + 1;
-			ant_near(3)(0) := ant(0) + 1; ant_near(3)(1) := ant(1) - 1;
-			ant_near(4)(0) := ant(0) - 1; ant_near(4)(1) := ant(1) + 1;
-			ant_near(5)(0) := ant(0);     ant_near(5)(1) := ant(1) - 1;
-			ant_near(6)(0) := ant(0) - 1; ant_near(6)(1) := ant(1);
-			ant_near(7)(0) := ant(0) - 1; ant_near(7)(1) := ant(1) - 1;
+		if(clk'event and clk = '0') then
+			ant_near := (
+				0 => (next_ant(0) + 1, next_ant(1) + 1),
+				1 => (next_ant(0) + 1, next_ant(1)),
+				2 => (next_ant(0),     next_ant(1) + 1),
+				3 => (next_ant(0) + 1, next_ant(1) - 1),
+				4 => (next_ant(0) - 1, next_ant(1) + 1),
+				5 => (next_ant(0),     next_ant(1) - 1),
+				6 => (next_ant(0) - 1, next_ant(1)),
+				7 => (next_ant(0) - 1, next_ant(1) - 1)
+				);
 			
-			if(goHome) then
-				best_way := 0;
+			if(goHome = true) then
 
-				for i in 1 to 7 loop
-					if(pheromone(ant_near(i)(0),ant_near(i)(0)) > pheromone(ant_near(i-1)(0),ant_near(i-1)(0))) then
-						best_way := i;
+				if(next_ant(0) = startPos(0) and next_ant(1) = startPos(1)) then -- hit on an anthill
+					goHome <= false;
+
+				else
+
+					best_way := 0;
+					for i in 1 to 7 loop
+						if(pheromone(ant_near(i)(0),ant_near(i)(1)) > pheromone(ant_near(best_way)(0),ant_near(best_way)(1))) then
+							best_way := i;
+							exit;
+						end if;
+					end loop;
+
+					if(pheromone(next_ant(0), next_ant(1)) > 0) then
+						pheromone(next_ant(0), next_ant(1)) <= pheromone(next_ant(0), next_ant(1)) - 1;
 					end if;
-				end loop;
 
-				if(pheromone(ant(0),ant(1)) > 0) then
-					--pheromone(ant(0),ant(1)) <= pheromone(ant(0),ant(1)) - 1;
+					next_ant <= (ant_near(best_way)(0), ant_near(best_way)(1));
 				end if;
 
-				ant <= (ant_near(best_way)(0), ant_near(best_way)(1));
-			else
+			elsif(goHome = false) then
+				if(eat(next_ant(0)-1,next_ant(1)-1) = true) then -- hit on an food
+					goHome <= true;
+					eat(next_ant(0)-1,next_ant(1)-1) <= false;
 
+				else
+
+					for i in 0 to 7 loop
+						if(pheromone(ant_near(i)(0),ant_near(i)(1)) >= 0) then
+							best_way := i;
+
+						end if;
+					end loop;
+					
+					for i in 1 to 7 loop
+						if((pheromone(ant_near(i)(0),ant_near(i)(1)) <= pheromone(ant_near(best_way)(0),ant_near(best_way)(1))) and (pheromone(ant_near(i)(0),ant_near(i)(1)) /= -1)) then
+							best_way := i;
+						end if;
+					end loop;
+
+					pheromone(next_ant(0), next_ant(1)) <= pheromone(next_ant(0), next_ant(1)) + 2;
+
+					next_ant <= (ant_near(best_way)(0), ant_near(best_way)(1));
+				end if;
 			end if;
 
+		elsif(clk'event and clk = '1') then
+			ant <= next_ant;
+			
+			-- result logger
+			write(outdata_line,next_ant(0));
+			write(outdata_line,string'(" "));
+			write(outdata_line,next_ant(1));
+			writeline(output_data_file,outdata_line);
+			--
 		end if;
 	end process;
 end struct;
--- course/types.vhd
+++ course/types.vhd
@@ -1,5 +1,10 @@
 package types is
 
-	type ant_arr is array (0 to 1) of integer;
+	constant MATRIX_SIZE: natural := 5 + 2;
+
+	type ant_arr is array (natural range 0 to 1) of integer range 0 to MATRIX_SIZE - 1;
+	type ant_near_arr is array (natural range 0 to 7) of ant_arr;
+	type pheromone_arr is array (natural range 0 to MATRIX_SIZE - 1, natural range 0 to MATRIX_SIZE - 1) of integer range -1 to 65535;
+	type eat_arr is array (natural range 0 to MATRIX_SIZE - 1 - 2, natural range 0 to MATRIX_SIZE - 1 - 2) of boolean;
 
 end types;
